47e56d7fc23321edbbce1909172a136c35696e75
Tono Lee <tajimanohamikinj-50@yahoo.co.jp>
2023-03-05 19:07:33 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function func1() {
+  var outputEchoit = document.getElementById("intext").value;
+  document.getElementById(
+    "show"
+  ).innerHTML = `<br><font size="50" face="impact"> ${outputEchoit}</font>`;
+}
